b694a61858d25dd8bb49b36a0c3f5c827d29689d
Marek Laskowski <marek.laskowski@matrix.senecac.on.ca>
2016-11-21 12:51:28 -0500

    started strings game example

---
+++ strings_game.c
@@ -0,0 +1,51 @@
+/*
+this program is a game that simulates a battle between a hero and a monster
+in this game the monster name is hard-coded (literal string)
+the player (hero) name will be entered by the user
+both the hero and the monster have Health Points (HP)
+the battle will continue until either the hero or monster has HP <= 0
+simulate the battle between the hero and monster:
+the characters take turns hitting each other 
+each character has a certain amount of damage they do to the other
+Damage is subracted from the approprate character's HP
+the winner will be the character with HP > 0
+
+cheat: by entering a secret hero name, the hero will get a lot stronger
+
+*/
+#include <stdio.h>
+//strlen, strcmp, strcpy, and strcat come from string.h
+#include <string.h>
+
+#define MAX_NAME_SIZE 100
+//function prototypes (headers)
+void printWelcomeScreen(void);
+void getHeroName(char name[]);
+
+int main(void){
+	char monsterName[] = "Freiza";
+	/*reserve enough space for hero name 100 characters or less +1 for the terminating null*/
+	char heroName[MAX_NAME_SIZE + 1] = "DEFAULT NAME";
+	//show intro screen
+	printWelcomeScreen();
+	//get the user's name
+	getHeroName(heroName);
+	//use strlen to get and display the number of characters hero's name
+	printf("DEBUG: user entered: %s, length: %d\n", heroName, strlen(heroName));
+
+	return 0;
+}
+
+void printWelcomeScreen(void){
+	printf("WELCOME TO DEAD ZONE\n");
+}
+
+
+void getHeroName(char name[]){
+	//prompt the user to enter the hero's name
+	printf("What epic name shall thou be known by.. brave warrior (%d characters maximum): ", MAX_NAME_SIZE);
+	//use scanf to copy the entered name into our array
+	//don't forget we #define'd MAX_NAME_SIZE as 100	
+	scanf("%100[^\n]",name); 
+
+}
